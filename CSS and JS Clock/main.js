3feb4ec2b25cce3da4efed68449c8ec20ffbcb1f
Tony <tonylu23@gmail.com>
2019-10-23 14:23:12 -0400

    Fix snapping of hands when seconds reset
    The hands will snap around when the seconds hand reaches 60 or 0 seconds. This fix simply speeds up the animation so the snap is not noticable.

--- CSS and JS Clock/main.js
+++ CSS and JS Clock/main.js
@@ -14,9 +14,22 @@ function setDate() {
     const minutesDegrees = ((minutes / 60) * 360) + 90;
     const secondsDegrees = ((seconds / 60) * 360) + 90;
 
+    checkIfSecondsResets(seconds);
+
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+}
+
+// prevent snapping of hands when seconds reaches 60/0
+function checkIfSecondsResets(seconds) {
+    const hands = document.querySelectorAll('.hand');
+    if (seconds == 0) {
+        hands.forEach(hand => hand.style.transitionDuration = '0s');
+    } else {
+        hands.forEach(hand => hand.style.transitionDuration = '0.05s');
+    }
 }
 
 setInterval(setDate, 1000);
