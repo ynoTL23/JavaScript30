fe9f6140dfb925013abf9a8f46d7f14aeeb9f3d9
Tony <tonylu23@gmail.com>
2019-10-28 15:40:33 -0400

    Implement exercises 4 - 7
    4 - Find the total years lived by all inventors
    5 - Sort inventors by years lived
    6 - Filter Boulevards in Paris that contain 'de'
    7 - Sort list of people alphabetically by last name

--- Array Cardio 1/main.js
+++ Array Cardio 1/main.js
@@ -37,15 +37,35 @@ console.table(ordered);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
+const totalYears = inventors.reduce((total, inventor) => {
+    return total + (inventor.passed - inventor.year);
+}, 0);
+console.log(totalYears);
 
 // 5. Sort the inventors by years lived
+const oldest = inventors.sort(function(a, b) {
+    const lastGuy = a.passed - a.year;
+    const nextGuy = b.passed - b.year;
+    return lastGuy > nextGuy ? -1 : 1;
+});
+console.table(oldest);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
-
+// const category = document.querySelector('.mw-category');
+// const links = Array.from(category.querySelectorAll('a'));
+// const de = links
+//                 .map(link => link.textContent)
+//                 .filter(streeName => streeName.includes('de'));
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+const alpha = people.sort((lastOne, nextOne) => {
+    const [alast, afirst] = lastOne.split(', ');
+    const [blast, bfirst] = nextOne.split(', ');
+    return alast > blast ? 1 : -1;
+});
+console.log(alpha);
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
