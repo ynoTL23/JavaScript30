0baf0013363d8cc7e0e7a17190ae83653e6613e9
Tony <tonylu23@gmail.com>
2019-10-31 21:35:44 -0400

    Make moles pop up and down indefinitely

--- Whack A Mole/main.js
+++ Whack A Mole/main.js
@@ -1,3 +1,35 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
+
+// generate random time from min to max, in milliseconds
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+// pick a random hole
+function randomHole(holes) {
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    // prevent the same hole being picked consecutively
+    if (hole == lastHole) {
+        console.log('Picked the same hole. Trying again...');
+        randomHole(holes);
+    }
+    lastHole = hole; // track previous hole to prevent consecutively picking same hole
+    return hole;
+}
+
+// make the mole pop up
+function peep() {
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+    hole.classList.add('up'); // mole comes up
+    setTimeout(() => {
+        hole.classList.remove('up'); // mole goes down after 'time' milliseconds
+        peep();
+    }, time)
+}
+
+peep();
